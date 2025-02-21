939c996baf1e4ecbb2cdc18406365c3090329fd7
Akpodigha Filatei <filatei@gtsng.com>
2025-02-21 20:15:13 +0100

    new commit

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
     console.log('✅ Connected to MongoDB');
   });
 
-  await app.listen(process.env.PORT ?? 3100, '0.0.0.0');
+  await app.listen(3300, '0.0.0.0');
 }
 bootstrap();
