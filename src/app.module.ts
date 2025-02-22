25ffc0ad2f4294d32832a021d1c5beb11adcde3b
Code By Omotosho <john.omotosho@yahoo.com>
2025-02-22 12:28:24 +0100

    Add firebase Auth Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -7,6 +7,7 @@ import { EstatesModule } from './estates/estates.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AiModule } from './ai/ai.module';
+import { FirebaseAuthMiddleware } from './auth/firebase-auth.middleware';
 
 @Module({
   imports: [
@@ -20,4 +21,8 @@ import { AiModule } from './ai/ai.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(FirebaseAuthMiddleware).forRoutes('*');
+  }
+}
